15289060256f552d86a0e3cb4e37f0ee73faf2af
GaegeSmith <gaege.smith@evsck12.com>
2021-09-22 12:36:19 -0500

    eod

--- Notes/unit1/sec2/StringLoops.java
+++ Notes/unit1/sec2/StringLoops.java
@@ -1,6 +1,7 @@
 public class StringLoops {
     public static void main(String[] args) {
-        System.out.println(howMany("", 'a'));
+        // System.out.println(howMany("", 'a'));
+
     }
     private static int howMany(String s, char chr) {
         int count = 0;
@@ -10,5 +11,8 @@ public class StringLoops {
             }
         }
         return count;
+    }
+    private static String removeDoubleSpace(String text) {
+        return "";
     }
 }
