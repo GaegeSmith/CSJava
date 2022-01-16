30740cf0c713c036562026cb9e2e836db420c796
InformalCleric <smithfamily6>
2022-01-15 20:44:54 -0600

    work has been done

--- Inheritance/homework/number 2/Checking.java
+++ Inheritance/homework/number 2/Checking.java
@@ -8,7 +8,7 @@ public class Checking extends Account {
     public Checking(int id, double startingBal, double overDraftLimit) {
         super(id, startingBal);
         this.OVER_DRAFT_LIMIT = overDraftLimit;
-        this.limitReached = startingBal > this.OVER_DRAFT_LIMIT;
+        this.limitReached = startingBal < -this.OVER_DRAFT_LIMIT;
     }
     public String toString() {
         return "Account id: " + this.getId() + "\n\tBalance: " + this.getBalance() + "\n\tOverdraft limit: " + this.OVER_DRAFT_LIMIT + "\n\tOverdraft limit reached: " + (this.getLimitReached()) + "\n\tDate opened: " + this.getDateCreated();
@@ -16,9 +16,12 @@ public class Checking extends Account {
     public boolean getLimitReached() {
         return this.limitReached;
     }
+    public boolean checkOverdraftLimit() {
+        return this.getBalance() < -this.OVER_DRAFT_LIMIT;
+    }
     @Override
     public void withdraw(double amount) {
-        if ((this.getBalance() - amount) > -this.OVER_DRAFT_LIMIT) {
+        if ((this.getBalance() - amount) < -this.OVER_DRAFT_LIMIT) {
             this.balance -= amount;
         } else {
             this.limitReached = true;
@@ -27,6 +30,6 @@ public class Checking extends Account {
     @Override
     public void deposit(double amount) {
         this.balance += amount;
-        this.limitReached = this.balance > this.OVER_DRAFT_LIMIT;
+        this.limitReached = this.getBalance() < -this.OVER_DRAFT_LIMIT;
     }
 }
--- Inheritance/homework/number 2/Savings.java
+++ Inheritance/homework/number 2/Savings.java
@@ -1,3 +1,3 @@
 public class Savings extends Account {
-    
+    public 
 }
